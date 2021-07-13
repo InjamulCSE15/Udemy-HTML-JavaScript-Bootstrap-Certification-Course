2351748af74308b50ca197df765e5389dc0983f6
unknown <yeasin.mohammod018@gmail.com>
2021-07-13 10:52:38 -0600

    57 JavaScript Strings

---
+++ JS-Required-Files/jsStrings.js
@@ -0,0 +1,7 @@
+// 57. JavaScript Strings:
+
+var studentName1 = "Sajjadul Alam";
+var studentName2 = "Injamul Alam";
+
+document.getElementById('strings').innerHTML = "<strong>Students Name: </strong><br>" +
+studentName1 + "<br>" + studentName2;
