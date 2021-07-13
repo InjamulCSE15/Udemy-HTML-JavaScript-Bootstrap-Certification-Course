ed22aa9c17f920b4a63e10cf0c6ae299b0b445d2
unknown <yeasin.mohammod018@gmail.com>
2021-07-13 06:28:46 -0700

    50 JavaScript Assignment Operator 51 JavaScript Arithmetic Operations and 52 JS Arithmetic Operations Continued has been completed

--- JS-Required-Files/jsAssignmentOperator.js
+++ JS-Required-Files/jsAssignmentOperator.js
@@ -1,3 +1,41 @@
-
+// 50. JavaScript Assignment Operator:
 var numHats = 30 + " Hats";
 document.getElementById('example').innerHTML =  numHats;
+
+// 51. JavaScript Arithmetic Operations:
+
+// Addition Operation
+var x = 100 + 50; 
+document.getElementById('add').innerHTML = "Addition of two values (100 + 50) are " + x;
+
+// Multiplication define two variables
+var a = 3;
+var b = (20 + 30) * a;
+
+document.getElementById('multiply').innerHTML = "Multiplication of two variables a and b are " + b;
+
+// Subtraction Operation
+
+var c = 150 - 50;
+document.getElementById('sub').innerHTML = "Subtraction of two values (150 and 50) are " + c;
+
+// Division Operation
+
+var d = 150 / 50;
+document.getElementById('divide').innerHTML = "Division of two numbers (150 and 50) are " + d;
+
+// 52. JavaScript Arithmetic Operations Continued
+
+//  Increment Operator: 
+
+var i = 5;
+i++;    
+var j = i;
+document.getElementById('inc').innerHTML = "Increment value 5 from to  " + j;
+
+// Decrement Operator:
+
+var h = 5;
+h--;
+var k = h;
+document.getElementById('dec').innerHTML = "Decrement value 5 from to " + k;
