0d140297e947670b5b2c9dea33a51b6f4a548716
unknown <yeasin.mohammod018@gmail.com>
2021-07-13 10:09:01 -0600

    54 JavaScript Data Types Done

--- JS-Required-Files/jsDataTypes.js
+++ JS-Required-Files/jsDataTypes.js
@@ -2,9 +2,23 @@
 // 01. Numbers:
 
 var age = 24;
-console.log(typeof(age));
+console.log(typeof(age)); // Output: number
 
-// 02. Strings
+// 02. Strings:
 
 var lastName = "Ismam";
-console.log(typeof(lastName));
+console.log(typeof(lastName)); // Output: string
+
+// 03. Arrays:
+
+var cars = ["BMW", "Volvo", "Ford"];
+console.log(typeof(cars));
+
+// 04. Object:
+ var person = {
+     firstName: "Injamul",
+     lastName: "Alam",
+     age: 50
+ }
+
+ console.log(typeof(person)); // Output: object
