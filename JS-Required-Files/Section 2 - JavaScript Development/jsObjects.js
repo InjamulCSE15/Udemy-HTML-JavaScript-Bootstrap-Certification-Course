b5b8adb1b6846dd8ed6c0d15f9e3bb39f9976801
unknown <yeasin.mohammod018@gmail.com>
2021-07-13 10:44:15 -0600

    56 JavaScript Object Output done

--- JS-Required-Files/Section 2 - JavaScript Development/jsObjects.js
+++ JS-Required-Files/Section 2 - JavaScript Development/jsObjects.js
@@ -9,3 +9,15 @@ var car = {
 
 document.getElementById('objType').innerHTML = car.type;
 document.getElementById('objModel').innerHTML = car.model;
+
+// 56. JavaScript Object Output:
+
+var person = {
+    firstName: "Injamul",
+    lastName: "Alam",
+    age: 60,
+    eyeColor: "brown"
+}
+
+document.getElementById('demo').innerHTML = 
+person.firstName + " has " + person.eyeColor + " eyes.";
