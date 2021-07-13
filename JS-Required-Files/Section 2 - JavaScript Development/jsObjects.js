52fdc4d34a45dddbea99b6bfd0cdf2eff9ccaa68
unknown <yeasin.mohammod018@gmail.com>
2021-07-13 10:37:33 -0600

    55 JavaScript Objects done

---
+++ JS-Required-Files/Section 2 - JavaScript Development/jsObjects.js
@@ -0,0 +1,11 @@
+// 55. JavaScript Objects:
+
+// Creating an objects:
+var car = {
+    type: "Acura",
+    model: "Integra",
+    color: "white"
+}
+
+document.getElementById('objType').innerHTML = car.type;
+document.getElementById('objModel').innerHTML = car.model;
