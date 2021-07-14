7755611b60ed3a577b33b885c09d20fe14f4385f
unknown <yeasin.mohammod018@gmail.com>
2021-07-14 12:51:44 -0600

    61 JavaScript Min and Max Function done

--- JS-Required-Files/jsMin&MaxFunction.js
+++ JS-Required-Files/jsMin&MaxFunction.js
@@ -4,3 +4,8 @@
 
 document.getElementById('minFunction').innerHTML = "Minimum number is: " + 
 Math.min(600, 300, 50, 75, -5, -150);
+
+// Max Function:
+
+document.getElementById('maxFunction').innerHTML = "Maximum number is: " + 
+Math.max(600, 300, 50, 75, -5, -150);
