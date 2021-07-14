046889ad60c6dbe048ba9a8903256da807d6241d
unknown <yeasin.mohammod018@gmail.com>
2021-07-14 13:20:55 -0600

    62 js math round

--- JS-Required-Files/jsMathRound.js
+++ JS-Required-Files/jsMathRound.js
@@ -1,6 +1,12 @@
 // 62. JavaScript Math Round Function:
 
-document.getElementById('mathRound1').innerHTML = Math.round(15.4); // Output : 15
+document.getElementById('mathRound1').innerHTML =
+"Using Math.round function (15.4) after  " + Math.round(15.4); // Output : 15
 
-document.getElementById('mathRound2').innerHTML = Math.round(15.6); // Output : 16
+document.getElementById('mathRound2').innerHTML = 
+"Using Math.round function (15.6) after " + Math.round(15.6); // Output : 16
 
+// Math.floor Function:
+
+document.getElementById('mathFloor').innerHTML = 
+"Using Math.floor Function (15.6) after " + Math.floor(15.6); // Output : 15
