5789ec5a660d1c7514c4d4f8f72400b2eada83c1
unknown <yeasin.mohammod018@gmail.com>
2021-07-14 13:50:31 -0600

    js arrays

--- JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Arrays/jsArrays.js
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Arrays/jsArrays.js
@@ -1,1 +1,6 @@
 // 63. JavaScript Arrays:
+
+var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
+document.getElementById('demo0').innerHTML = fruits[0]; // Output: Apple
+
+document.getElementById('demo1').innerHTML = fruits[1]; // Output: Orange
