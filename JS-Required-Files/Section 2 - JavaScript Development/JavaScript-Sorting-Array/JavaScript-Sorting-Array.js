36eda39f3d7e177967aecf1ae35cd6a10d9e65bc
unknown <yeasin.mohammod018@gmail.com>
2021-07-17 02:09:31 +0600

    68 JavaScript Sorting an Array Start

---
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Sorting-Array/JavaScript-Sorting-Array.js
@@ -0,0 +1,9 @@
+// 68. JavaScript Sorting an Array:
+
+/* 
+sort():
+sort() method to delete existing elements, insert new elements, and replace elements in an array 
+*/
+
+var fruits = ["Apple", "Orange", "Mango", "Pear"];
+fruits.splice(2, 0, "Lemon", "Banana");
