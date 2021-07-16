264d3458dc0a9d832494a002f867051f80629115
unknown <yeasin.mohammod018@gmail.com>
2021-07-16 05:04:41 -0600

    64 JavaScript Array Attributes

--- JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Arrays/jsArrays.js
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Arrays/jsArrays.js
@@ -6,3 +6,15 @@ document.getElementById('demo0').innerHTML = fruits[0]; // Output: Apple
 document.getElementById('demo1').innerHTML = fruits[1]; // Output: Orange
 
 document.getElementById('length').innerHTML = fruits.length; // Output: 7
+
+
+// 64. JavaScript Array Attributes:
+ 
+var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
+document.getElementById('arrayAttributes').innerHTML = fruits.toString(); // Output: Apple,Orange,Grape,Mango,Banana,Jack fruit,Pine-apple
+
+// If we separated each string use join() method:
+
+var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
+document.getElementById('join').innerHTML = fruits.join(" * ");
+// Output: Apple * Orange * Grape * Mango * Banana * Jack fruit * Pine-apple
