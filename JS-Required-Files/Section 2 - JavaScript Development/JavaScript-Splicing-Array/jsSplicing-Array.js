5dd211ed28ff0def836882cf21973fd072208eb8
unknown <yeasin.mohammod018@gmail.com>
2021-07-17 01:55:56 +0600

    star

--- JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Splicing-Array/jsSplicing-Array.js
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Splicing-Array/jsSplicing-Array.js
@@ -5,3 +5,12 @@ splice():
 splice() method to delete existing elements, insert new elements, and replace elements in an array 
 */
 
+var fruits = ["Apple", "Orange", "Mango", "Pear"];
+fruits.splice(2, 0, "Lemon", "Banana");
+
+/*
+ The value 2 indicates that we want the new elements to start with an index number of 2 
+ the 0 indicates 
+ */
+document.getElementById('splice').innerHTML = fruits.join(", ");
+// Output: Apple, Orange, Lemon, Banana, Mango, Pear
