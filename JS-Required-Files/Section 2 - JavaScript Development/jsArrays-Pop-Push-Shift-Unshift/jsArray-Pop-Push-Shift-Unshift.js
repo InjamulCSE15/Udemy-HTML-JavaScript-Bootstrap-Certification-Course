8f4db8043800a626089eb17c8ac6f579ef31b4ae
unknown <yeasin.mohammod018@gmail.com>
2021-07-16 05:38:42 -0600

    Array Push operation done

--- JS-Required-Files/Section 2 - JavaScript Development/jsArrays-Pop-Push-Shift-Unshift/jsArray-Pop-Push-Shift-Unshift.js
+++ JS-Required-Files/Section 2 - JavaScript Development/jsArrays-Pop-Push-Shift-Unshift/jsArray-Pop-Push-Shift-Unshift.js
@@ -1,6 +1,6 @@
 // 65. JavaScript Arrays Pop - Push - Shift - Unshift:
 
-// Pop:
+// Pop():
 
 var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
 fruits.pop(); // Last element will be removed from the array
@@ -9,10 +9,18 @@ document.getElementById('pop').innerHTML = fruits.join(", ");
 // Output: Apple, Orange, Grape, Mango, Banana, Jack fruit
 
 
-// Shift:
+// Shift():
 
 var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
 fruits.shift(); // First element will be removed form the array
 
 document.getElementById('shift').innerHTML = fruits.join(", "); 
 // Output: Orange, Grape, Mango, Banana, Jack fruit, Pine-apple
+
+// Push():
+
+var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
+fruits.push("Cherry"); // Add a new element will be last position of the array
+
+document.getElementById('push').innerHTML = fruits.join(", "); 
+// Output: Orange, Grape, Mango, Banana, Jack fruit, Pine-apple, Cherry
