361f5805a25d3f9ffbd73064dfbd1cafbfb1ff2b
unknown <yeasin.mohammod018@gmail.com>
2021-07-17 02:23:40 +0600

    68 JavaScript Sorting an Array is completed

--- JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Sorting-Array/JavaScript-Sorting-Array.js
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Sorting-Array/JavaScript-Sorting-Array.js
@@ -2,8 +2,22 @@
 
 /* 
 sort():
-sort() method to delete existing elements, insert new elements, and replace elements in an array 
+The sort() method sorts the elements of a given array in a specific ascending or descending order.
 */
 
-var fruits = ["Apple", "Orange", "Mango", "Pear"];
-fruits.splice(2, 0, "Lemon", "Banana");
+var cars = ["Volvo", "Ford", "Toyota", "Lexus"];
+cars.sort();
+
+document.getElementById('sorting').innerHTML = cars;
+/* Output: Ford,Lexus,Toyota,Volvo
+Alphabetically A to Z sorted the array elements.
+*/
+
+// Reverse():
+var cars = ["Volvo", "Ford", "Toyota", "Lexus", "BMW", "Viper"];
+cars.reverse();
+
+document.getElementById('reversed').innerHTML = cars;
+/* Output: Viper,BMW,Lexus,Toyota,Ford,Volvo
+Reversed all of the elements from the array
+*/
