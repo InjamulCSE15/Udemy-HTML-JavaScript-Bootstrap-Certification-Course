e97a7bae51438ac6a7af34e148764a14f57a246c
unknown <yeasin.mohammod018@gmail.com>
2021-07-16 05:32:54 -0600

    Array shift operation done

--- JS-Required-Files/Section 2 - JavaScript Development/jsArrays-Pop-Push-Shift-Unshift/jsArray-Pop-Push-Shift-Unshift.js
+++ JS-Required-Files/Section 2 - JavaScript Development/jsArrays-Pop-Push-Shift-Unshift/jsArray-Pop-Push-Shift-Unshift.js
@@ -3,7 +3,16 @@
 // Pop:
 
 var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
-fruits.pop(); // last element will be removed
+fruits.pop(); // Last element will be removed from the array
 
 document.getElementById('pop').innerHTML = fruits.join(", "); 
 // Output: Apple, Orange, Grape, Mango, Banana, Jack fruit
+
+
+// Shift:
+
+var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
+fruits.shift(); // First element will be removed form the array
+
+document.getElementById('shift').innerHTML = fruits.join(", "); 
+// Output: Orange, Grape, Mango, Banana, Jack fruit, Pine-apple
