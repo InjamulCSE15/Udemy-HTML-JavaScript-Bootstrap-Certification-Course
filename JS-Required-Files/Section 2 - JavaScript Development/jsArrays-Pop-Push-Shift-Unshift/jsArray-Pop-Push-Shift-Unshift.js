456777f9fefc06a3559de144c7c634af0497b017
unknown <yeasin.mohammod018@gmail.com>
2021-07-16 05:28:52 -0600

    65 JavaScript Array Pop Push Shift and Unshift

---
+++ JS-Required-Files/Section 2 - JavaScript Development/jsArrays-Pop-Push-Shift-Unshift/jsArray-Pop-Push-Shift-Unshift.js
@@ -0,0 +1,9 @@
+// 65. JavaScript Arrays Pop - Push - Shift - Unshift:
+
+// Pop:
+
+var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
+fruits.pop(); // last element will be removed
+
+document.getElementById('pop').innerHTML = fruits.join(", "); 
+// Output: Apple, Orange, Grape, Mango, Banana, Jack fruit
