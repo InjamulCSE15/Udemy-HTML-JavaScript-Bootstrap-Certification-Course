8356961ac5d881250d58f3c956e86db8f7a1a8c4
unknown <yeasin.mohammod018@gmail.com>
2021-07-16 05:58:03 -0600

    66 JavaScript Changing & Deleting an array element from an array

---
+++ JS-Required-Files/JavaScript-Changing-&-Deleting/JavaScript-Changing-&-Deleting.js
@@ -0,0 +1,9 @@
+// 66. JavaScript Changing & Deleting:
+
+// Changing:
+
+var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
+fruits[0] = "Kiwi"; // First element will be replace from the array
+
+document.getElementById('changing').innerHTML = fruits.join(", "); 
+// Output: Kiwi, Orange, Grape, Mango, Banana, Jack fruit
