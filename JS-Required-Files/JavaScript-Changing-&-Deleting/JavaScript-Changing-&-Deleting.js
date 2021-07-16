22fd7814ed8ead918f05aba0fb9c458934d752b1
unknown <yeasin.mohammod018@gmail.com>
2021-07-16 06:35:09 -0600

    correction done

--- JS-Required-Files/JavaScript-Changing-&-Deleting/JavaScript-Changing-&-Deleting.js
+++ JS-Required-Files/JavaScript-Changing-&-Deleting/JavaScript-Changing-&-Deleting.js
@@ -3,7 +3,7 @@
 // Changing:
 
 var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
-fruits[0] = "Kiwi"; // First element will be replace from the array
+fruits[0] = "Kiwi"; // First element will be changed from the array
 
 document.getElementById('changing').innerHTML = fruits.join(", "); 
 // Output: Kiwi, Orange, Grape, Mango, Banana, Jack fruit
