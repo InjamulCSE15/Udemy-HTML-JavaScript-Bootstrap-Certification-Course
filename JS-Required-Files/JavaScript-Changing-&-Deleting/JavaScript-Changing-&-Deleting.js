d0aac1b9eb731788f4d90b42fec8e3dc4ef62698
unknown <yeasin.mohammod018@gmail.com>
2021-07-16 07:03:51 -0600

    66 JavaScript Changing & Deleting Complete

--- JS-Required-Files/JavaScript-Changing-&-Deleting/JavaScript-Changing-&-Deleting.js
+++ JS-Required-Files/JavaScript-Changing-&-Deleting/JavaScript-Changing-&-Deleting.js
@@ -7,3 +7,11 @@ fruits[0] = "Kiwi"; // First element will be changed from the array
 
 document.getElementById('changing').innerHTML = fruits.join(", "); 
 // Output: Kiwi, Orange, Grape, Mango, Banana, Jack fruit
+
+// Deleting:
+
+var fruits = ["Apple", "Orange", "Grape", "Mango", "Banana", "Jack fruit", "Pine-apple"];
+delete fruits[2]; // Delete element from the 3rd index of the array
+
+document.getElementById('deleting').innerHTML = fruits.join(", "); 
+// Output: Apple, Orange, , Mango, Banana, Jack fruit, Pine-apple
