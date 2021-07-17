790f6b47feac8828d13b9c51724437af7c90f091
unknown <yeasin.mohammod018@gmail.com>
2021-07-17 19:26:30 +0600

    77 JavaScript Break and Continue Done

--- JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Break-and-Continue/JavaScript-Break-and-Continue.js
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Break-and-Continue/JavaScript-Break-and-Continue.js
@@ -4,11 +4,44 @@ We can use a javascript break and continue statements to either exit a loop or s
 Let's start with the break statement in this example.
 
 */
+
+// Break:
 var i;
+var result = " ";
 for (i = 0; i < 10; i++) {
     if(i === 3) {
         break;
     }
-    document.write(i+"<br>");
+    result += i + "<br>";
 }
-document.getElementById('Break').innerHTML = text;
+document.getElementById('Break').innerHTML = result;
+/* Output:
+This is Break code output:
+0
+1
+2
+*/
+
+// Continue:
+var j;
+var output  = " ";
+for (j = 0; j < 10; j++) {
+    if(j === 3) {
+        continue;
+    }
+    output += j + "<br>";
+}
+document.getElementById('Continue').innerHTML = output;
+/*
+Output:
+This is Continue code output:
+0
+1
+2
+4
+5
+6
+7
+8
+9
+*/
