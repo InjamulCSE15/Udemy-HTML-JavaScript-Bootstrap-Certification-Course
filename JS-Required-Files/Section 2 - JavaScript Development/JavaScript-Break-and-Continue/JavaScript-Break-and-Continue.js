aed47b0e7d046ba5755557fa352317df6446af7f
unknown <yeasin.mohammod018@gmail.com>
2021-07-17 18:59:59 +0600

    77 JavaScript Break Example done

---
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Break-and-Continue/JavaScript-Break-and-Continue.js
@@ -0,0 +1,14 @@
+/* 77. JavaScript Break and Continue:
+We can use a javascript break and continue statements to either exit a loop or skipping iteration of a loop.
+
+Let's start with the break statement in this example.
+
+*/
+var i;
+for (i = 0; i < 10; i++) {
+    if(i === 3) {
+        break;
+    }
+    document.write(i+"<br>");
+}
+document.getElementById('Break').innerHTML = text;
