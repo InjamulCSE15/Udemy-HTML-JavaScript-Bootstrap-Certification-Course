be8a339ed893ea642d168f3beed306e03eb3fe6b
unknown <yeasin.mohammod018@gmail.com>
2021-07-17 17:56:34 +0600

    76 JavaScript Do While loop Done

--- JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Do-While-Loop/JavaScript-Do-While-Loop.js
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-Do-While-Loop/JavaScript-Do-While-Loop.js
@@ -1,2 +1,16 @@
-// 76. JavaScript Do-While Loop:
+/* 76. JavaScript Do-While Loop:
+The do-while loop is a variation of the while loop a do while loop will always execute the entire code block once before checking if a condition is met if the condition is met. After the first pass it will then loop the code block as long as the condition remains true.
 
+Example:
+*/
+
+function myFunction() {
+    var i = 0;
+    do {
+        document.write(i+"<br>");
+        i++;
+    }
+    while (i < 10){
+        document.getElementById('doWhile').innerHTML = text;       
+    }
+}
