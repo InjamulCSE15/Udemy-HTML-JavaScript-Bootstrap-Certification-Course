89dcd1a169d179f0663471b2ed6f8fd81d3b4b76
unknown <yeasin.mohammod018@gmail.com>
2021-07-17 17:36:33 +0600

    75 JavaScript While Loop done

--- JS-Required-Files/Section 2 - JavaScript Development/JavaScript-While-Loops/JavaScript-While-Loops.js
+++ JS-Required-Files/Section 2 - JavaScript Development/JavaScript-While-Loops/JavaScript-While-Loops.js
@@ -1,1 +1,11 @@
 // 75. JavaScript While Loops:
+
+var i = 0;
+while (i < 20) {
+    document.write(i);
+    document.write("<br>");
+    i++;   
+}
+/*
+Here we've instructed our script to output the value of i as long as the value doesn't exceed 20 the while loop will loop over and over 19 times in this case each time it will output the value of i started at 0 and incremented by 1.
+*/
