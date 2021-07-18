42f86a8fb292dbbfb700c8cc0b75d34bcba5def6
unknown <yeasin.mohammod018@gmail.com>
2021-07-18 17:51:58 +0600

    80 JavaScript Project 1 BG Color Changer Done

---
+++ JS-Required-Files/Section 2 - JavaScript Development/Lecture 39 - JS Project 1 l BG Color Changer/jsProject1.js
@@ -0,0 +1,6 @@
+// 80. JavaScript Project-1 BG Color Changer:
+
+
+var bgColor = new Array("#029FD4", "#D43702", "#1EFF00", "#EAEAFF", "#FFEA00", "#AB5252", "#FFFFFF", "#DE28D8")
+
+document.body.style.background=bgColor[Math.floor(Math.random()*bgColor.length)]
